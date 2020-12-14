1a582296f3be526346a54d944a4d5d51ea696875
shibmish <shibmish@adobe.com>
2020-12-14 08:23:27 -0800

    added grafana dashboard

--- loadgen/script.sh
+++ loadgen/script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-docker run -i loadimpact/k6 run --vus 2 --duration 300s - <loadgen/io-external.js >> io-external.log &
-docker run -i loadimpact/k6 run --vus 2 --duration 300s - <loadgen/east-west.js >> east-west.log &
-docker run -i loadimpact/k6 run --vus 2 --duration 300s - <loadgen/ethos-public.js >> ethos-public.log &
-docker run -i loadimpact/k6 run --vus 2 --duration 300s - <loadgen/sample-delay.js >> sample-delay.log &
-docker run -i loadimpact/k6 run --vus 2 --duration 300s - <loadgen/foo-friend.js >> foo-friend.log &
+rm -rf *.log
+duration=300s
+users=3
+docker run -i loadimpact/k6 run --vus $users --duration $duration - <loadgen/io-external.js >> io-external.log &
+docker run -i loadimpact/k6 run --vus $users --duration $duration - <loadgen/east-west.js >> east-west.log &
+docker run -i loadimpact/k6 run --vus $users --duration $duration - <loadgen/ethos-public.js >> ethos-public.log &
+docker run -i loadimpact/k6 run --vus $users --duration $duration - <loadgen/sample-delay.js >> sample-delay.log &
+docker run -i loadimpact/k6 run --vus $users --duration $duration - <loadgen/foo-friend.js >> foo-friend.log &
